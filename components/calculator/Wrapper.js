f5c42c604d71728340281c94cec928d138b17815
Christopher Kapic <christopherkapic@gmail.com>
2021-10-09 23:49:28 -0500

    cleaner

--- components/calculator/Wrapper.js
+++ components/calculator/Wrapper.js
@@ -1,17 +1,9 @@
-// import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useContext, useEffect } from 'react';
-
-//import Button from '@/components/ui/Button';
-// import Input from '@/components/ui/Input';
-// import LoadingDots from '@/components/ui/LoadingDots';
-// import Logo from '@/components/icons/Logo';
 import { useUser } from '@/utils/useUser';
 import { getData } from '@/utils/helpers';
-
 import styles from '../../styles/Calculator.module.css';
 import { is_test } from '@/utils/is-test';
-
 import Graph from '@/components/calculator/Graph';
 
 // Uncomment for testing
@@ -69,7 +61,6 @@ const Wrapper = () => {
     red: '#C75959',
     backgroundColor: '#cccccc'
   }
-
 
   return (
       <>
--- pages/calculator/[ticker].js
+++ pages/calculator/[ticker].js
@@ -1,21 +1,3 @@
-// import Link from 'next/link';
-// import { useRouter } from 'next/router';
-// import { useEffect } from 'react';
-
-//import Button from '@/components/ui/Button';
-// import Input from '@/components/ui/Input';
-// import LoadingDots from '@/components/ui/LoadingDots';
-// import Logo from '@/components/icons/Logo';
-// import { useUser } from '@/utils/useUser';
-// import { getData } from '@/utils/helpers';
-
-// import styles from '../../styles/Calculator.module.css';
-// import { is_test } from '@/utils/is-test';
-
-// import Graph from '@/components/calculator/Graph';
-
-// Uncomment for testing
-// import { testdata } from '@/utils/test-data'
 import DataStore from '@/components/context/DataStore';
 import InputStore from '@/components/context/InputStore';
 import Wrapper from '@/components/calculator/Wrapper';
